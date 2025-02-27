d7886fbead07852c0c7875c33ad5bf80b5021452
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-02-27 14:13:29 +0100

    EX Java OOP Shop (incapsulamento)

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,18 +2,24 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-        Prodotto primoProdotto = new Prodotto("nome", "descrizione", 19.99, 22);
+        Prodotto primoProdotto = new Prodotto("nome", "descrizione", 19.99);
+        Prodotto secondoProdotto = new Prodotto("nomeDue", 14.99);
 
-        System.out.println(primoProdotto.code);
-        System.out.println(primoProdotto.name);
-        System.out.println(primoProdotto.description);
-        System.out.println(primoProdotto.price);
-        System.out.println(primoProdotto.iva);
+        System.out.println("Primo prodotto:");
+        System.out.println(primoProdotto.getCode());
+        System.out.println(primoProdotto.getName());
+        System.out.println(primoProdotto.getPrice() + "€");
+        System.out.println(primoProdotto.getPriceWithIva() + "€");
+        System.out.println(primoProdotto.getExtendedName());
 
-        System.out.println(primoProdotto.priceWithoutIva() + "€");
-        System.out.println(primoProdotto.priceWithIva() + "€");
-        System.out.println(primoProdotto.extendedName());
+        System.out.println(" ");
 
+        System.out.println("Secondo prodotto:");
+        System.out.println(secondoProdotto.getCode());
+        System.out.println(primoProdotto.getName());
+        System.out.println(secondoProdotto.getPrice() + "€");
+        System.out.println(secondoProdotto.getPriceWithIva() + "€");
+        System.out.println(secondoProdotto.getExtendedName());
 
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,43 +1,77 @@
 package org.lessons.java.shop;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Random;
 
 public class Prodotto {
     // * Attributi
-    int code;
-    String name;
-    String description;
-    double price;
-    int iva;
+    private int code;
+    private String name;
+    private String description;
+    private double price;
+    private static int iva = 22;
 
-    // * costruttore 
-    public Prodotto( String name, String description, double price, int iva ){
+    // * costruttore 1
+    public Prodotto( String name, String description, double price ){
         Random randomNumber = new Random();
 
         this.code = randomNumber.nextInt(9999);
         this.name = name;
         this.description = description;
         this.price = price;
-        this.iva = iva;
+    }
+
+    // * costruttore 2
+    public Prodotto(String name, double price) {
+        Random randomNumber = new Random();
+        this.code = randomNumber.nextInt(9999);
+
+        this.name = name;
+        description = "";
+        this.price = price;
+
     }
 
     // * Metodi
-    // prezzo senza iva
-    double priceWithoutIva() {
+    // codice
+    public int getCode () {
+        return code;
+    }
+
+    // nome
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    // descrizione
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription( String descriprion ) {
+        this.description = descriprion;
+    }
+
+    // prezzo
+    public double getPrice() {
         return price;
     }
 
+    public void setPrice( double price ) {
+        this.price = price;
+    }
+
     // prezzo con iva
-    double priceWithIva() {
-        
+    public double getPriceWithIva() {
         return Math.round(price + (price / 100 * iva));
     }
 
     // nome per esteso
-    String extendedName() {
-        return name + code;
+    public String getExtendedName() {
+        return name + "-" + code;
     }
 
 }
